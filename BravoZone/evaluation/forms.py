dbedfecc27510936963a28063f05e066feb0006d
Basma <129796403+basiix1@users.noreply.github.com>
2023-09-10 20:58:22 +0300

    up2

--- BravoZone/evaluation/forms.py
+++ BravoZone/evaluation/forms.py
@@ -4,4 +4,8 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['name', 'content', 'start_time', 'end_time', 'points', 'status']
+        fields = ['name', 'content', 'start_time', 'end_time', 'points', 'status', 'assigned_to']
+        widgets = {
+            'start_time': forms.DateTimeInput(format=('%Y-%m-%dT%H:%M'), attrs={'type': 'datetime-local'}),
+            'end_time': forms.DateTimeInput(format=('%Y-%m-%dT%H:%M'), attrs={'type': 'datetime-local'})
+        }
