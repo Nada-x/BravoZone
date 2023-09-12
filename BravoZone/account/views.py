0e3dd0382e19ef74a103bd733e7bd5a04204130e
Nada Almutairi <nadaamusaad@gmail.com>
2023-09-12 12:01:27 +0300

    Last update

--- BravoZone/account/views.py
+++ BravoZone/account/views.py
@@ -47,7 +47,7 @@ def login_view(request):
                 if user is not None:
                     login(request, user)
                     if user.is_superuser:
-                        return redirect("account:all_emploee")
+                        return redirect("account:home")
                     else:
                         return redirect('evaluation:task_list')
                 else:
